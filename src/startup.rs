c02a7a441896f2ee336865dcec83c8455e4ad26b
Jordan Rhodes <jsrhodes15@gmail.com>
2023-12-09 22:01:40 -0500

    Start logging details

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,18 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::web::Data;
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // wrap connection in a smart pointer (Arc)
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     // connection is available in closure
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // get a pointer copy and attach to app state
