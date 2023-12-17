e0da75259c6d48de31e90c3f5d6c9eafbfc5971d
Jordan Rhodes <jsrhodes15@gmail.com>
2023-12-16 23:05:55 -0700

    Refactor to allow for hierarchical env config, add docker

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ use std::net::TcpListener;
 async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("falcon-external".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-
     // panic if we can't read config
     let configuration = get_configuration().expect("Failed to read config");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+
+    let connection_pool =
+        PgPool::connect_lazy(
+            &configuration.database.connection_string().expose_secret()
+        )
         .expect("Failed to connect to database");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     // Bubble up the io::Error if we failed to bind address
     run(listener, connection_pool)?.await
